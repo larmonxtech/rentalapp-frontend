e18313bbdb169d287a116ebfda6f21004c5b5e7d
MarlexJL <marlonlinarez@outlook.com.pe>
2026-05-12 05:52:19 -0500

    add CRUD de Categories

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
 import { CategoryComponent } from './pages/category/category.component';
 import { ExamComponent } from './pages/exam/exam.component';
+import { CategoryEditComponent } from './pages/category/category-edit/category-edit.component';
 
 export const routes: Routes = [
-    { path: 'pages/category', component: CategoryComponent},
+    { 
+        path: 'pages/category', component: CategoryComponent,
+        children: [
+            { path: 'new', component: CategoryEditComponent },
+            { path: 'edit/:id', component: CategoryEditComponent },
+        ],
+    },
     { path: 'pages/exam', component: ExamComponent }
 ];
